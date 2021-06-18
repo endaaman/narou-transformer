1f012405439c0da81958f43093d7b881d1679893
endaaman <endaaman@frontier.hokudai.ac.jp>
2021-06-18 13:03:01 +0900

    seq2seq

--- models.py
+++ models.py
@@ -1,20 +1,24 @@
 import math
+
+from torch import Tensor
+from torch.nn import Transformer, TransformerEncoder, TransformerEncoderLayer
+import math
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.nn import TransformerEncoder, TransformerEncoderLayer
 
+
+UNK_IDX, PAD_IDX, BOS_IDX, EOS_IDX = 0, 1, 2, 3
 
 class PositionalEncoding(nn.Module):
-    def __init__(self, d_model, dropout=0.1, max_len=5000):
+    def __init__(self, emb_size, dropout=0.1, max_len=5000):
         super(PositionalEncoding, self).__init__()
         self.dropout = nn.Dropout(p=dropout)
-
-        pe = torch.zeros(max_len, d_model)
-        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
-        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
-        pe[:, 0::2] = torch.sin(position * div_term)
-        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = torch.zeros(max_len, emb_size)
+        pos = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, emb_size, 2).float() * (-math.log(10000.0) / emb_size))
+        pe[:, 0::2] = torch.sin(pos * div_term)
+        pe[:, 1::2] = torch.cos(pos * div_term)
         pe = pe.unsqueeze(0).transpose(0, 1)
         self.register_buffer('pe', pe)
 
@@ -22,9 +26,20 @@ class PositionalEncoding(nn.Module):
         x = x + self.pe[:x.size(0), :]
         return self.dropout(x)
 
-class TransformerModel(nn.Module):
+
+class TokenEmbedding(nn.Module):
+    def __init__(self, vocab_size: int, emb_size):
+        super(TokenEmbedding, self).__init__()
+        self.embedding = nn.Embedding(vocab_size, emb_size)
+        self.emb_size = emb_size
+
+    def forward(self, tokens: Tensor):
+        return self.embedding(tokens.long()) * math.sqrt(self.emb_size)
+
+
+class AutoEncTransformerModel(nn.Module):
     def __init__(self, ntoken, ninp, nout, nhead, nhid, nlayers, dropout=0.5):
-        super(TransformerModel, self).__init__()
+        super().__init__()
         self.model_type = 'Transformer'
         self.pos_encoder = PositionalEncoding(ninp, dropout)
         encoder_layers = TransformerEncoderLayer(ninp, nhead, nhid, dropout)
@@ -34,11 +49,6 @@ class TransformerModel(nn.Module):
         self.decoder = nn.Linear(ninp, nout)
 
         self.init_weights()
-
-    def generate_square_subsequent_mask(self, sz):
-        mask = (torch.triu(torch.ones(sz, sz)) == 1).transpose(0, 1)
-        mask = mask.float().masked_fill(mask == 0, float('-inf')).masked_fill(mask == 1, float(0.0))
-        return mask
 
     def init_weights(self):
         initrange = 0.1
@@ -54,7 +64,73 @@ class TransformerModel(nn.Module):
         return output
 
 
-if __name__ == '__main__':
+
+# Seq2Seq Network
+class Seq2SeqTransformer(nn.Module):
+    def __init__(self,
+                 num_encoder_layers: int,
+                 num_decoder_layers: int,
+                 emb_size: int,
+                 nhead: int,
+                 src_vocab_size: int,
+                 tgt_vocab_size: int,
+                 dim_feedforward: int = 512,
+                 dropout: float = 0.1):
+        super(Seq2SeqTransformer, self).__init__()
+        self.transformer = Transformer(d_model=emb_size,
+                                       nhead=nhead,
+                                       num_encoder_layers=num_encoder_layers,
+                                       num_decoder_layers=num_decoder_layers,
+                                       dim_feedforward=dim_feedforward,
+                                       dropout=dropout)
+        self.generator = nn.Linear(emb_size, tgt_vocab_size)
+        self.src_tok_emb = TokenEmbedding(src_vocab_size, emb_size)
+        self.tgt_tok_emb = TokenEmbedding(tgt_vocab_size, emb_size)
+        self.positional_encoding = PositionalEncoding(
+            emb_size, dropout=dropout)
+
+    def forward(self,
+                src: Tensor,
+                trg: Tensor,
+                src_mask: Tensor,
+                tgt_mask: Tensor,
+                src_padding_mask: Tensor,
+                tgt_padding_mask: Tensor,
+                memory_key_padding_mask: Tensor):
+        src_emb = self.positional_encoding(self.src_tok_emb(src))
+        tgt_emb = self.positional_encoding(self.tgt_tok_emb(trg))
+        outs = self.transformer(src_emb, tgt_emb, src_mask, tgt_mask, None,
+                                src_padding_mask, tgt_padding_mask, memory_key_padding_mask)
+        return self.generator(outs)
+
+    def encode(self, src: Tensor, src_mask: Tensor):
+        return self.transformer.encoder(self.positional_encoding(
+                            self.src_tok_emb(src)), src_mask)
+
+    def decode(self, tgt: Tensor, memory: Tensor, tgt_mask: Tensor):
+        return self.transformer.decoder(self.positional_encoding(
+                          self.tgt_tok_emb(tgt)), memory,
+                          tgt_mask)
+
+
+def generate_square_subsequent_mask(sz):
+    mask = (torch.triu(torch.ones(sz, sz)) == 1).transpose(0, 1)
+    mask = mask.float().masked_fill(mask == 0, float('-inf')).masked_fill(mask == 1, float(0.0))
+    return mask
+
+
+def create_mask(src, tgt):
+    src_seq_len = src.shape[0]
+    tgt_seq_len = tgt.shape[0]
+
+    tgt_mask = generate_square_subsequent_mask(tgt_seq_len)
+    src_mask = torch.zeros((src_seq_len, src_seq_len)).type(torch.bool)
+
+    src_padding_mask = (src == PAD_IDX).transpose(0, 1)
+    tgt_padding_mask = (tgt == PAD_IDX).transpose(0, 1)
+    return src_mask, tgt_mask, src_padding_mask, tgt_padding_mask
+
+def test_autoenc():
     # ntokens = len(vocab.stoi) # the size of vocabulary
     ntokens = 100
     ninp = 200 # embedding dimension
@@ -63,14 +139,14 @@ if __name__ == '__main__':
     nlayers = 2 # the number of nn.TransformerEncoderLayer in nn.TransformerEncoder
     nhead = 2 # the number of heads in the multiheadattention models
     dropout = 0.2 # the dropout value
-    model = TransformerModel(ntokens, ninp, nout, nhead, nhid, nlayers, dropout)
+    model = AutoEncTransformerModel(ntokens, ninp, nout, nhead, nhid, nlayers, dropout)
 
     bptt = 35
     input_tensor = (torch.randn(35, 10) + 3).type(torch.LongTensor)
 
-    src_mask = model.generate_square_subsequent_mask(bptt)
+    src_mask = generate_square_subsequent_mask(bptt)
     if input_tensor.size(0) != bptt:
-        src_mask = model.generate_square_subsequent_mask(input_tensor.size(0))
+        src_mask = generate_square_subsequent_mask(input_tensor.size(0))
 
     print('in')
     print(input_tensor.size(), src_mask.size())
@@ -79,3 +155,44 @@ if __name__ == '__main__':
     print('out')
     print(output_tensor.size())
     print(output_tensor.view(-1, nout).size())
+
+def test_seq2seq():
+    SRC_VOCAB_SIZE = 1000
+    TGT_VOCAB_SIZE = 1001
+    EMB_SIZE = 512
+    NHEAD = 8
+    FFN_HID_DIM = 512
+    BATCH_SIZE = 128
+    NUM_ENCODER_LAYERS = 3
+    NUM_DECODER_LAYERS = 3
+
+    model = Seq2SeqTransformer(NUM_ENCODER_LAYERS, NUM_DECODER_LAYERS, EMB_SIZE,
+                                     NHEAD, SRC_VOCAB_SIZE, TGT_VOCAB_SIZE, FFN_HID_DIM)
+
+    src_tensor = torch.ones(35, 10).type(torch.LongTensor)
+    tgt_tensor = torch.ones(100, 10).type(torch.LongTensor)
+
+    src_mask, tgt_mask, src_padding_mask, tgt_padding_mask = create_mask(src_tensor, tgt_tensor)
+
+    logits = model(
+        src_tensor,
+        tgt_tensor,
+
+        src_mask,
+        tgt_mask,
+
+        src_padding_mask,
+        tgt_padding_mask,
+
+        src_padding_mask)
+
+    print('in')
+    print(src_tensor.size(), src_mask.size())
+    print(src_tensor.type(), src_mask.type())
+    print('out')
+    print(logits.size())
+
+
+
+if __name__ == '__main__':
+    test_seq2seq()
